fa30412ead15391c53d2b2c9f6bb2f5d5687da17
Dmitry "Ventan" Teplov <Ventantogame@gmail.com>
2021-03-05 21:47:01 +0100

    tried to fix menu mobile

--- js/loadManager.js
+++ js/loadManager.js
@@ -1,4 +1,3 @@
 window.onload=function (){
-    console.log("loaded2");
     document.getElementById("loader").style.display = "none";
 };
--- js/menuManager.js
+++ js/menuManager.js
@@ -12,87 +12,71 @@ function reportWindowSize() {
         }
     }
 }
+var step1 = 0;
+var step2 = 0;
+var step3 = 0;
+var step4 = 0;
+
+var interval1;
+var interval2;
+var interval3;
+var interval4;
 
 window.onresize = reportWindowSize;
 
 bureger.addEventListener("click",openMenu);
 function openMenu(){
+    clearInterval(interval1);
+    clearInterval(interval2);
+    clearInterval(interval3);
+    clearInterval(interval4);
+    step1=0;
+    step2=0;
+    step3=0;
+    step4=0;
 
-    if(!menuOpen){
-        upline.animate(
-            [
-                { transform: 'rotate(45deg) translate(0, 34.54px)' }
-            ],{
-                duration: 400,
-                direction: "normal",
-                fill: "forwards"
-            }
-        )
-        mid.animate(
-            [
-                { transform: 'rotate(-45deg) translate(0, 34.54px )'  }
-            ],{
-                duration: 400,
-                direction: "normal",
-                fill: "forwards"
-            }
-        );
-        downline.animate(
-            [
-                {   opacity: '0%'}
-            ],{
-                duration: 400,
-                direction: "normal",
-                fill: "forwards"
-            }
-        );
-        navmb.animate(
-            [
-                {   transform: 'translateX(0)'}
-            ],{
-                duration: 400,
-                direction: "normal",
-                fill: "forwards"
-            }
-        );
-        menuOpen=true;
-    }else{
-        upline.animate(
-            [
-                { transform: 'rotate(0) translate(0, 20px)' }
-            ],{
-                duration: 400,
-                direction: "normal",
-                fill: "forwards"
-            }
-        )
-        mid.animate(
-            [
-                { transform: 'rotate(0) translate(0, 40px)'  }
-            ],{
-                duration: 400,
-                direction: "normal",
-                fill: "forwards"
-            }
-        );
-        downline.animate(
-            [
-                {   opacity: '100%'}
-            ],{
-                duration: 400,
-                direction: "normal",
-                fill: "forwards"
-            }
-        );
-        navmb.animate(
-            [
-                {   transform: 'translateX(40vw)'}
-            ],{
-                duration: 400,
-                direction: "normal",
-                fill: "forwards"
-            }
-        );
-        menuOpen=false;
+    interval1 = setInterval(function (){mainrot(menuOpen);},5);
+    interval2 = setInterval(function (){secondrot(menuOpen);},5);
+    interval3 = setInterval(function (){thirdopacity(menuOpen);},5);
+    interval4 = setInterval(function (){displaymenu(menuOpen);},5);
+    menuOpen=!menuOpen;
+}
+function mainrot(direction){
+    var temp = !direction ? 45 - step1 : step1;
+    upline.style.transform = "rotate("+temp+"deg) translate(0, "+(20+temp*0.3231111)+"px)";
+    upline.style.webkitTransform = "rotate("+temp+"deg) translate(0, "+(20+temp*0.3231111)+"px)";
+    if((temp===45 && step1 !== 0) || (temp === 0 && step1 !== 0)){
+        clearInterval(interval1);
+        step1=0;
     }
+    step1++;
+}
+function secondrot(direction){
+    var temp = !direction ? 45 - step2 : step2;
+    mid.style.transform = "rotate("+(-1*temp)+"deg) translate(0, "+(40+temp*-0.1213333)+"px)";
+    mid.style.webkitTransform = "rotate("+(-1*temp)+"deg) translate(0, "+(40+temp*-0.1213333)+"px)";
+    if((temp===45 && step2 !== 0) || (temp === 0 && step2 !== 0)){
+        clearInterval(interval2);
+        step2=0;
+    }
+    step2++;
+}
+function thirdopacity(direction){
+    var temp = direction ? 50 - step3 : step3;
+    downline.style.opacity = 2*temp+"%";
+    if((temp===50 && step3 !== 0) || (temp === 0 && step3 !== 0)){
+        clearInterval(interval3);
+        step3=0;
+    }
+    step3++;
+}
+function displaymenu( direction){
+    var temp = direction ? 45 - step4 : step4;
+    navmb.style.transform = "translateX("+temp+"vw)";
+    navmb.style.webkitTransform = "translateX("+temp+"vw)";
+    if((temp===45 && step4 !== 0) || (temp === 0 && step4 !== 0)){
+        clearInterval(interval4);
+        step4=0;
+    }
+    step4++;
 }
